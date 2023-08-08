be003cff66adcc75b629e536997bdb4cb119e2eb
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-08-08 20:01:12 -0300

    preparacion de entorno

---
+++ index.ts
@@ -0,0 +1,14 @@
+// comando para transformar codigo ts a js 
+//tsc index.ts
+// en el package.json escribo lo siguiente
+//"start:"tsc index.ts && node index.js"
+// para poder convertir mi archivo a codigo js y que ejecute el codigo 
+
+const age = 20
+
+if (age >= 18) {
+  console.log("Es mayor de edad");
+} else {
+  console.log("Es menor de edad");
+
+}
